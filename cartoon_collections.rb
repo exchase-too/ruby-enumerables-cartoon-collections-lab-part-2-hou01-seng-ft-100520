7fb3d0d626633e3ebbe6bedb098f5021d3d016a2
Eric Smith <eric.c.smith@sbcglobal.net>
2020-09-19 20:40:00 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -8,7 +8,6 @@ end
 def summon_captain_planet(planeteer_calls)
   # Use an Enumerable to capitalize and add '!' to every element in the passed in array
   # Return a new array of the results
-  #new_array = planeteer_calls.each {|i| i.upcase + "!"}
   new_array = planeteer_calls.map{|i| i.capitalize + "!"}
   new_array
 end
@@ -16,6 +15,7 @@ end
 def long_planeteer_calls(planeteer_calls)
   # Use an Enumerable to check if any string in the passed in array is greater than 4 characters long
   # Return the boolean result of this check
+  new_array = planeteer_calls.any?{|i| i.length > 4}
 end
 
 def find_valid_calls(planeteer_calls)
